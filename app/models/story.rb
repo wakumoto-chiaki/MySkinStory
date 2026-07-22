e9a53f808c6c1d6f151735bce2a11d5c0c97ca4a
wakumoto chiaki <chiakiwakumoto0719@gmail.com>
2026-07-22 13:05:53 +0900

    ストーリー開始日からのDay表示機能を実装

--- app/models/story.rb
+++ app/models/story.rb
@@ -4,4 +4,8 @@ class Story < ApplicationRecord
 
   validates :title, presence: true
   validates :started_on, presence: true
+
+  def elapsed_days
+    (Date.current - started_on).to_i + 1
+  end
 end
