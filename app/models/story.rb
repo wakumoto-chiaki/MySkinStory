e9ab2b29da56949f01a9a4768af6e58c327d1d5d
wakumoto chiaki <chiakiwakumoto0719@gmail.com>
2026-07-22 18:31:28 +0900

    Before After取得処理をStoryモデルへ移動

--- app/models/story.rb
+++ app/models/story.rb
@@ -8,4 +8,12 @@ class Story < ApplicationRecord
   def elapsed_days
     (Date.current - started_on).to_i + 1
   end
+
+  def before_record
+    records.order(:recorded_on).first
+  end
+
+  def after_record
+    records.order(:recorded_on).last
+  end
 end
