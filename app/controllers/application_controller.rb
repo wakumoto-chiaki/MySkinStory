a1f0a174292e3d9e9071d179d8d77604a5333b4c
wakumoto chiaki <chiakiwakumoto0719@gmail.com>
2026-07-24 12:21:57 +0900

    共通レイアウト追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -7,7 +6,15 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def after_sign_in_path_for(resource)
+    stories_path
+  end
+
+  def after_sign_up_path_for(resource)
+    stories_path
   end
 end
